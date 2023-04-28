c81b81cacc89585587fba67c5e73582a8097a3e0
pvt-Tron <pv.tassi@gmail.com>
2023-04-28 04:33:35 +0200

    corrections

---
+++ Module 10.5/F_Way_Too_Long_Words.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int nl;
+    char s[1001];
+    char longst[1001];
+    scanf("%d",&nl);
+    for(int line=1;line<=nl;line=line+1)
+    {
+        scanf("%s",&s);
+        
+    }
+    int lenstrig=strlen(s);
+    if(lenstrig<=4)
+    {
+        printf("%s",s);
+    }
+    else
+    {
+        char longst[1001]=s;
+        for(int i=0;i<lenstrig;i=i+1)
+        {
+
+        }        
+    }
+    
+return 0;
+
+}
