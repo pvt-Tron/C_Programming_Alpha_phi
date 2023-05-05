f1c6779f2c584d82bcb84ba1e1a92882798fbad1
pvt-Tron <pv.tassi@gmail.com>
2023-05-05 18:07:52 +0200

    15

--- Module 13 Loop Rec/sorting.c
+++ Module 13 Loop Rec/sorting.c
@@ -8,13 +8,13 @@ int main()
     {
         scanf("%d",&a[i]);
     }
-    for(int i=0;i<n-1;i++)
+    for(int i=1;i<n-1;i++)
     {
         for(int j=i+1;j<n;j++)
         {
         //     printf("%d %d\n",a[i],a[j]);
         // }
-            if(a[i]>a[j])
+            if(a[i]<a[j])
             {
                 int temp=a[i];
                 a[i]=a[j];
